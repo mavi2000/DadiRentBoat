69efd6af7ccb2a196d13197bbf601319071a6897
Muhammad Haris <haristahir2020@gmail.com>
2024-04-20 11:20:49 +0500

    again commit

--- FrontEnd/src/Components/Checkout/Checkout.jsx
+++ FrontEnd/src/Components/Checkout/Checkout.jsx
@@ -4,8 +4,22 @@ const Checkout = () => {
   return (
     <div className='mx-[6%] mt-[2%]'>
       <h1 className='font-medium text-3xl text-[#000000]'>Check Out</h1>
-      <p className='para-book mt-[1%]'>Book your rental in two simple steps. <br />
+      <p className='para-book mt-2'>Book your rental in two simple steps. <br />
       Renting boats from us means relying on a competent team that takes care of all the pre and post boat rental phases. But not only that, all our boats are periodically checked and tested according to current regulations. This is why boat rental with DaDi Rent is safe. Not only in the price but also in the quality of our boats and in the service that we are able to provide always and in any case.</p>
+    
+    <div className=' flex space-x-[2%]'>
+      <div className=' w-[35%] bg-slate-400'> 
+        hello shadola
+      </div>
+
+      <div className='w-[65%] bg-red-500'>
+        hello ck
+      </div>
+
+    </div>
+
+    
+    
     </div>
   )
 }
