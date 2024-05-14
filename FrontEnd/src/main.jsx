be71cd58389957812bc5344fc7fe5d5ffc4cf3b1
amirsuliman01 <amirsuliman01@gmail.com>
2024-05-14 12:36:13 +0500

    added dummy.css

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import './assets/styles/variables.css';
-import './assets/styles/hero-bgs.css';
+import '../styles/hero-bgs.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
