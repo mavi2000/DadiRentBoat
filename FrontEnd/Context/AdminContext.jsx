6551fb3162837eae94539b09f05f96e03c54c9fd
Huzaifa <76128718+Huzaifa4472@users.noreply.github.com>
2024-05-21 13:10:49 +0500

    "Updated BaseUrl.js to use localhost, fixed quotes and formatting in multiple files"

--- FrontEnd/Context/AdminContext.jsx
+++ FrontEnd/Context/AdminContext.jsx
@@ -47,12 +47,10 @@ const AdminProvider = ({ children }) => {
         "/condition/Term-condition",
         conditionData
       );
-      toast.success("Condition added successfully");
-      console.log(response.data);
+      // toast.success('Condition added successfully');
       return response.data;
     } catch (error) {
       setError(error.response?.data?.message || "Failed to add condition");
-      console.log(error);
       throw error;
     }
   };
