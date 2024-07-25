8fbb08588bb46f98339898fcff2659cc15a7bfc0
uzair1761 <ozairkhanan@gmail.com>
2024-07-25 15:36:04 +0500

    server checking

--- FrontEnd/src/Components/Layout.jsx
+++ FrontEnd/src/Components/Layout.jsx
@@ -1,7 +1,7 @@
 import Navbar from './Navbar/Navbar';
 import Footer from './Footer';
 import LocationMap from './LocationMap';
-
+// new changes
 const Layout = ({ Children, isLocation }) => {
   return (
     <>
