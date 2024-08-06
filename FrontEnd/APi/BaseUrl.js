f23a5866e30ad3e5f84bd93853585cf2fb9ecd20
uzair1761 <ozairkhanan@gmail.com>
2024-08-06 20:16:18 +0500

    live url

--- FrontEnd/APi/BaseUrl.js
+++ FrontEnd/APi/BaseUrl.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL = axios.create({
-  baseURL: "http://localhost:3800",
+  baseURL: "https://backend.dadirent.it",
   headers: {
     "Content-Type": "application/json",
   },
